c68fedd60ab9238c6accf3715c2efe266c2c2567
Марина Коновалова <konovalova.owl@gmail.com>
2021-06-22 23:57:58 +0500

    Create a view structure

--- places_remember/memories/views.py
+++ places_remember/memories/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+@login_required
+def add_memory(request):
+    return render(request, 'add_memory.html')
+
+
+@login_required
+def delete_memory(request):
+    return render(request, 'delete_memory.html')
